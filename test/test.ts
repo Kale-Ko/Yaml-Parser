6c89f016a217a30f0fbb1feb99c3000b162f248b
Kale-Ko <github@kaleko.dev>
2021-05-04 20:28:30 -0400

    Update

---
+++ test/test.ts
@@ -0,0 +1,7 @@
+const yamlParser = require("../bin/parser.js")
+
+var yaml = yamlParser.toYamlFromFile("./test/test.json", { encoding: "utf8" })
+var json = yamlParser.toJsonFromFile("./test/test.yml", { encoding: "utf8" })
+
+if (yaml.toString() == "string: String\nobject:\n  subString: Sub String\n  subObject:\n    subSubString: Sub Sub String\n  subArray:\n    - Sub 1.\n    - Sub 2.\n    - Sub 3.\narray:\n  - 1.\n  - 2.\n  - 3.\n") console.log("toYaml Working")
+if (JSON.stringify(json.toJson()) == JSON.stringify({ string: "String", object: { subString: "SubString", subObject: { subSubString: "SubSubString" }, subArray: ["Sub1.", "Sub2.", "Sub3."] }, array: ["1.", "2.", "3."] })) console.log("toJson Working")
