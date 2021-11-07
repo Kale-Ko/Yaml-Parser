93f8ca2689e5fc887d711d45f72025e84bd94f8d
Kale-Ko <github@kaleko.dev>
2021-11-06 20:02:04 -0400

    Update

--- dist/parser.js
+++ dist/parser.js
@@ -1,3 +1,9 @@
+/**
+    @license
+    MIT License
+    Copyright (c) 2021 Kale Ko
+    See https://kaleko.ga/license.txt
+*/
 var fs = require("fs");
 var detectIndent = require('detect-indent');
 var Json = (function () {
